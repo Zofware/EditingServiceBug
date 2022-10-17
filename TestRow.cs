152df4843d32f0d555e6317ba615a9d4dc1c5347
Adam Zofware <adam@zofware.com>
2022-10-17 09:16:17 -0700

    Fix bug in TestRow's MvvmProperty setter implementation.

--- TestRow.cs
+++ TestRow.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EditingServiceBug
 {
@@ -20,7 +16,7 @@ namespace EditingServiceBug
             {
                 if (value != MvvmPropertyValue)
                 {
-                    value = MvvmPropertyValue;
+                    MvvmPropertyValue = value;
                     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MvvmProperty)));
                 }
             }
